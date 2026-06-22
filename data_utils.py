680d10765f3e6968040dd98d6f50e7f8f53934c1
Ewan-Sutherland <ewansutherland@icloud.com>
2026-06-22 17:05:29 +0100

    Move files to correct folder

--- data_utils.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-
-def get_returns(ticker):
-    data = yf.download(ticker, start="2018-01-01", end="2025-12-31")
-    close = data["Close"].squeeze()
-    return close.pct_change().dropna()
