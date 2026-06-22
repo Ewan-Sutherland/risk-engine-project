822634da7eee07c6b7dbf40f0bfcc57529f9386b
Ewan-Sutherland <ewansutherland@icloud.com>
2026-06-22 16:24:50 +0100

    Initial commit of project

---
+++ BacktestVaR.py
@@ -0,0 +1,40 @@
+# Importing libraries
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from InverseNormal import inverseNormal
+from scipy.stats import t
+from data_utilse import get_returns
+
+# Creating Variables
+returns = get_returns("AAPL")
+rolling_mu = returns.rolling(250).mean()
+rolling_sigma = returns.rolling(250).std()
+
+# VaR calculation
+def VaR_Calc(confidence):
+    z = inverseNormal(1 - confidence)
+    VaR = -(rolling_mu+rolling_sigma*z)
+    return VaR
+
+# Calculate breach rate
+def breach_rate(p):
+    predicted_VaR = VaR_Calc(p)
+    breaches = returns.shift(-1) < -predicted_VaR
+    breach_rate = breaches.sum() / breaches.count()
+    return breach_rate
+
+print(breach_rate(0.95))
+print(breach_rate(0.99))
+
+
+
+df, loc, scale = t.fit(returns)
+
+z_t = t.ppf(0.01, df) * np.sqrt((df - 2) / df)
+predicted_VaR_t = -(rolling_mu + rolling_sigma * z_t)
+
+breaches_t = returns.shift(-1) < -predicted_VaR_t
+breach_rate_t = breaches_t.sum() / breaches_t.count()
+
+print(breach_rate_t)
---
+++ VaRCalculator.py
@@ -0,0 +1,39 @@
+# Importing libraries
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from InverseNormal import inverseNormal
+from scipy.stats import t
+from data_utilse import get_returns
+
+# Creating Variables
+returns = get_returns("AAPL")
+mu = returns.mean()
+sigma = returns.std()
+
+# VaR calculation
+def VaR_Calc(confidence):
+    z = inverseNormal(1 - confidence)
+    VaR = -(mu+sigma*z)
+    return VaR
+
+print(VaR_Calc(0.95))
+print(VaR_Calc(0.99))
+
+# Monte Carlo simulation
+def monte_carlo(N):
+    rng = np.random.default_rng()
+    sims = rng.normal(mu, sigma, N)
+    VaR_95 = -np.percentile(sims, 5)
+    VaR_99 = -np.percentile(sims, 1)
+    return VaR_95, VaR_99
+
+print(monte_carlo(100_000))
+print(monte_carlo(1_000_000))
+
+df, loc, scale = t.fit(returns)
+sims_t = t.rvs(df, loc=loc, scale=scale, size=1_000_000)
+VaR_95_t = -np.percentile(sims_t, 5)
+VaR_99_t = -np.percentile(sims_t, 1)
+
+print(VaR_95_t, VaR_99_t)
